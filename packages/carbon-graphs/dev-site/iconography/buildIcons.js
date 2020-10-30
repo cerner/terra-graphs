598870bc6cc6cdc97da0d31681f93f687ffc9bf4
Saad <saadadnan94@gmail.com>
2020-10-30 12:30:25 -0500

    removed extra lines

--- packages/carbon-graphs/dev-site/iconography/buildIcons.js
+++ packages/carbon-graphs/dev-site/iconography/buildIcons.js
@@ -1,7 +1,5 @@
 'use strict';
 
-
-
 const fs = require('fs');
 const path = require('path');
 const prettier = require('prettier');
--- packages/carbon-graphs/dev-site/serve/serve-static.js
+++ packages/carbon-graphs/dev-site/serve/serve-static.js
@@ -1,5 +1,3 @@
-
-
 const express = require('express');
 const rateLimit = require('express-rate-limit');
 const fs = require('fs');
