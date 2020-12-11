9a8bc160aff7086049e8067a939c84e9f19c5775
Harold Vilander <harold.vilander@gmail.com>
2020-12-11 07:31:05 -0600

    batch-load-content: removed private from util

--- packages/carbon-graphs/src/js/helpers/constructUtils.js
+++ packages/carbon-graphs/src/js/helpers/constructUtils.js
@@ -9,7 +9,6 @@ import utils from './utils';
  * Simple utility that checks if the input is an array or objects or simple object.
  * Iterate over the array and calls the task callback function.
  *
- * @private
  * @param {object|Array} input - Input can be a single object or an array of objects
  * @param {Function} task - task callback function that will be called with the input as an argument
  * @returns {undefined} returns nothing
