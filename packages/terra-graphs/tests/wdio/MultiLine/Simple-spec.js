85967255203bf40d40bcc1144b7d7d7df9f1c095
AshishMotanamGurunadham <motanamashish@gmail.com>
2021-02-26 09:39:24 -0600

    Added all automated tests

--- packages/terra-graphs/tests/wdio/MultiLine/Simple-spec.js
+++ packages/terra-graphs/tests/wdio/MultiLine/Simple-spec.js
@@ -1,13 +1,14 @@
 Terra.describeViewports('Simple multi line', ['tiny', 'medium', 'large'], () => {
     describe('Validates', () => {
-      before(() => browser.url('/raw/tests/terra-graphs/multi-line/simple'));
+      beforeEach(() => browser.url('/raw/tests/terra-graphs/multi-line/simple'));
   
-      it('canvas element', () => {
-        Terra.validates.screenshot('canvas', { selector: '.carbon-graph-canvas' });
+      it('canvas element dataset', () => {   
+        browser.pause(750*6);   
+        Terra.validates.screenshot('canvas ', { selector: '.carbon-graph-canvas' });
       });
-  
       it('legend element', () => {
-          Terra.validates.screenshot('legend', { selector: '.carbon-legend' });
+        browser.pause(750*6);   
+        Terra.validates.screenshot('legend', { selector: '.carbon-legend' });
         });
     });
-  });
+  });x
