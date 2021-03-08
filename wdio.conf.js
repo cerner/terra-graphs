a83e241e9fcaa20f0f51bdf8ead46b7d70339ea3
AshishMotanamGurunadham <motanamashish@gmail.com>
2021-03-08 13:11:17 -0600

    Functional testing

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,16 +1,14 @@
 const path = require('path');
-const defaultWdioConfig = require('terra-toolkit/config/wdio/wdio.conf');
-
-const wdioConfig = defaultWdioConfig.config;
+const { config } = require('@cerner/terra-functional-testing/lib/config/wdio.conf');
 
 if (process.env.npm_package_name !== 'terra-graphs') {
-  wdioConfig.specs = [path.join(__dirname, 'packages', process.env.npm_package_name, 'tests', 'wdio', '**', '*-spec.js')];
+  config.specs = [path.join(__dirname, 'packages', process.env.npm_package_name, 'tests', 'wdio', '**', '*-spec.js')];
 }
 
 const travis = process.env.TRAVIS;
 
 if (travis) {
-  wdioConfig.host = 'localhost';
+  config.host = 'localhost';
 }
 
-exports.config = wdioConfig;
+exports.config = config;
