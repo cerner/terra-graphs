6425caacb819e7a40c4003076ec0699b32165301
AshishMotanamGurunadham <motanamashish@gmail.com>
2021-04-23 13:45:16 -0500

    Made changes to travis.yml file to fix build

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,18 +1,8 @@
 const path = require('path');
 const { config } = require('@cerner/terra-functional-testing/lib/config/wdio.conf');
 
-config.serviceOptions = {
-  seleniumVersion: '3.141.59-20210311',
-};
-
 if (process.env.npm_package_name !== 'terra-graphs') {
   config.specs = [path.join(__dirname, 'packages', process.env.npm_package_name, 'tests', 'wdio', '**', '*-spec.js')];
-}
-
-const travis = process.env.TRAVIS;
-
-if (travis) {
-  config.host = 'localhost';
 }
 
 exports.config = config;
