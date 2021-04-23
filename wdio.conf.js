a32ba13d509186fb294fd43f79413d66f4c03548
AshishMotanamGurunadham <motanamashish@gmail.com>
2021-04-23 12:54:20 -0500

    Added service options in wdio.conf.js file to fix ci build

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const { config } = require('@cerner/terra-functional-testing/lib/config/wdio.conf');
 
+config.serviceOptions = {
+  seleniumVersion: '3.141.59-20210311',
+};
+
 if (process.env.npm_package_name !== 'terra-graphs') {
   config.specs = [path.join(__dirname, 'packages', process.env.npm_package_name, 'tests', 'wdio', '**', '*-spec.js')];
 }
