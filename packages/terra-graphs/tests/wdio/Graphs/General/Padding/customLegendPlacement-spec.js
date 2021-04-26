d6516885a0a0abb83ef885fe42ab7cbc3ed393f4
AshishMotanamGurunadham <motanamashish@gmail.com>
2021-04-26 15:34:25 -0500

    Made chnages to snapshots to fix build

--- packages/terra-graphs/tests/wdio/Graphs/General/Padding/customLegendPlacement-spec.js
+++ packages/terra-graphs/tests/wdio/Graphs/General/Padding/customLegendPlacement-spec.js
@@ -2,12 +2,8 @@ Terra.describeViewports('Custom Legend Placement', ['tiny', 'medium', 'large'], 
   describe('Validates', () => {
     before(() => browser.url('/raw/tests/terra-graphs/graphs/general/padding/custom-legend-placement'));
 
-    it('canvas element', () => {
-      Terra.validates.screenshot('canvas', { selector: '.carbon-graph-canvas' });
-    });
-
-    it('legend element', () => {
-      Terra.validates.screenshot('legend', { selector: '.carbon-legend' });
+    it('container with reduced padding', () => {
+      Terra.validates.screenshot('canvas', { selector: '.bindto-container' });
     });
   });
 });
--- packages/terra-graphs/tests/wdio/Graphs/General/Padding/graphAndLegendPaddingReduced-spec.js
+++ packages/terra-graphs/tests/wdio/Graphs/General/Padding/graphAndLegendPaddingReduced-spec.js
@@ -2,12 +2,10 @@ Terra.describeViewports('Graph and legend padding reduced', ['tiny', 'medium', '
   describe('Validates', () => {
     before(() => browser.url('/raw/tests/terra-graphs/graphs/general/padding/graph-and-legend-padding-reduced'));
 
-    it('canvas element', () => {
-      Terra.validates.screenshot('canvas', { selector: '.carbon-graph-canvas' });
-    });
-
-    it('legend element', () => {
-      Terra.validates.screenshot('legend', { selector: '.carbon-legend' });
+    it('container with reduced padding', () => {
+      const element = $('.carbon-axis-y');
+      element.moveTo();
+      Terra.validates.screenshot('canvas', { selector: '.bindto-container' });
     });
   });
 });
