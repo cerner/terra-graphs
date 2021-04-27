887f1d18b42218e5fabd82627ecabc431df4d202
Saad <saadadnan94@gmail.com>
2021-04-27 12:32:51 -0500

    moved docker to travis file

--- wdio.conf.js
+++ wdio.conf.js
@@ -5,10 +5,4 @@ if (process.env.npm_package_name !== 'terra-graphs') {
   config.specs = [path.join(__dirname, 'packages', process.env.npm_package_name, 'tests', 'wdio', '**', '*-spec.js')];
 }
 
-const travis = process.env.TRAVIS;
-
-if (travis) {
-  config.host = 'localhost';
-}
-
 exports.config = config;
