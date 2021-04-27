24a4b68694a3b9cfd84dda9ca73ffc9228d2935f
Saad Adnan <38024451+sdadn@users.noreply.github.com>
2021-04-27 10:31:55 -0500

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { config } = require('@cerner/terra-functional-testing/lib/config/wdio.conf');
+const { config } = require('@cerner/terra-functional-testing');
 
 if (process.env.npm_package_name !== 'terra-graphs') {
   config.specs = [path.join(__dirname, 'packages', process.env.npm_package_name, 'tests', 'wdio', '**', '*-spec.js')];
