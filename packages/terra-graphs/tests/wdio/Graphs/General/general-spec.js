5b4cf178a32e1b0e79e144b5d88647de70aa1ff0
AshishMotanamGurunadham <motanamashish@gmail.com>
2021-05-07 13:02:31 -0500

    Made changes to wdio tests

--- packages/terra-graphs/tests/wdio/Graphs/General/general-spec.js
+++ packages/terra-graphs/tests/wdio/Graphs/General/general-spec.js
@@ -1,16 +1,16 @@
 Terra.describeViewports('General', ['tiny', 'medium', 'large'], () => {
   describe('Axes', () => {
     it('validates datetime bucket graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/general/axes/datetime-bucket');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/general/axes/datetime-bucket');
       Terra.validates.screenshot('datetime_bucket', { selector: '.carbon-graph-container' });
     });
 
     it('validates label hidden graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/general/axes/label-hidden');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/general/axes/label-hidden');
       Terra.validates.screenshot('label_hidden', { selector: '.carbon-graph-container' });
     });
     describe('Label truncation', () => {
-      beforeEach(() => browser.url('/raw/tests/terra-graphs/graphs/general/axes/label-truncation'));
+      beforeEach(() => browser.url('/raw/tests/terra-graphs-docs/graphs/general/axes/label-truncation'));
       it('validates label truncation graph container', () => {
         Terra.validates.screenshot('label_truncation_with_no_click', { selector: '.carbon-graph-container' });
       });
@@ -32,90 +32,90 @@ Terra.describeViewports('General', ['tiny', 'medium', 'large'], () => {
     });
 
     it('validates static x-axis ticks graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/general/axes/static-x-axis-ticks');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/general/axes/static-x-axis-ticks');
       Terra.validates.screenshot('static_x_axis_ticks', { selector: '.carbon-graph-container' });
     });
 
     it('validates supress tick values trailing zero graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/general/axes/suppress-Tick-Value-Trailing-Zero');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/general/axes/suppress-Tick-Value-Trailing-Zero');
       Terra.validates.screenshot('supress_tick_values_trailing_zero', { selector: '.carbon-graph-container' });
     });
 
     it('validates x-axis alternate locale graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/general/axes/x-axis-alternate-locale');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/general/axes/x-axis-alternate-locale');
       Terra.validates.screenshot('x-axis_alternate_locale', { selector: '.carbon-graph-container' });
     });
 
     it('validates x-axis hidden graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/general/axes/x-axis-hidden');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/general/axes/x-axis-hidden');
       Terra.validates.screenshot('x-axis_hidden ', { selector: '.carbon-graph-container' });
     });
 
     it('validates x-axis orientation top graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/general/axes/x-axis-orientation-top');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/general/axes/x-axis-orientation-top');
       Terra.validates.screenshot('x-axis_orientation_top', { selector: '.carbon-graph-container' });
     });
 
     it('validates x-axis ticks formatted graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/general/axes/x-axis-ticks-formatted');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/general/axes/x-axis-ticks-formatted');
       Terra.validates.screenshot('x-axis_ticks_formatted', { selector: '.carbon-graph-container' });
     });
 
     it('validates Y-axis hidden graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/general/axes/y-axis-hidden');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/general/axes/y-axis-hidden');
       Terra.validates.screenshot('y-axis_hidden', { selector: '.carbon-graph-container' });
     });
   });
 
   describe('Grid', () => {
     it('validates horizontal grid hidden graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/general/grid/horizontal-grid-hidden');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/general/grid/horizontal-grid-hidden');
       Terra.validates.screenshot('horizontal_grid_hidden', { selector: '.carbon-graph-container' });
     });
 
     it('validates vertical grid hidden graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/general/grid/vertical-grid-hidden');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/general/grid/vertical-grid-hidden');
       Terra.validates.screenshot('vertical_grid_hidden', { selector: '.carbon-graph-container' });
     });
   });
 
   describe('Legend', () => {
     it('validates legend hidden graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/general/legend/legend-hidden');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/general/legend/legend-hidden');
       Terra.validates.screenshot('legend_hidden', { selector: '.carbon-graph-container' });
     });
 
     it('validates legend item disable graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/general/legend/legend-item-disable');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/general/legend/legend-item-disable');
       Terra.validates.screenshot('legend_item-disable', { selector: '.carbon-graph-container' });
     });
 
     it('validates legend with legend options graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/general/legend/line-with-legend-option');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/general/legend/line-with-legend-option');
       Terra.validates.screenshot('legend_with_legend_options', { selector: '.carbon-graph-container' });
     });
   });
 
   describe('Other', () => {
     it('validates background color graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/general/other/background-color');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/general/other/background-color');
       Terra.validates.screenshot('background_color', { selector: '.carbon-graph-container' });
     });
 
     it('validates no data graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/general/other/no-data');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/general/other/no-data');
       Terra.validates.screenshot('no_data', { selector: '.carbon-graph-container' });
     });
   });
 
   describe('Legend', () => {
     it('validates custom legend placement graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/general/padding/custom-legend-placement');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/general/padding/custom-legend-placement');
       Terra.validates.screenshot('custom_legend_placement', { selector: '.bindto-container' });
     });
 
     it('validates graph and legend padding reduced graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/general/padding/graph-and-legend-padding-reduced');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/general/padding/graph-and-legend-padding-reduced');
       const element = $('.carbon-axis-y');
       element.moveTo();
       Terra.validates.screenshot('graph_and_legend_padding_reduced', { selector: '.bindto-container' });
--- packages/terra-graphs/tests/wdio/Graphs/LineGraph/line-graph-spec.js
+++ packages/terra-graphs/tests/wdio/Graphs/LineGraph/line-graph-spec.js
@@ -1,66 +1,66 @@
 Terra.describeViewports('LineGraph ', ['tiny', 'medium', 'large'], () => {
   describe('Basic line graphs', () => {
     it('validates dashed line graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/line-graph/basic-line-graphs/dashed-line');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/basic-line-graphs/dashed-line');
       Terra.validates.screenshot('dashed_line', { selector: '.carbon-graph-container' });
     });
 
     it('validates disable calibration graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/line-graph/basic-line-graphs/disable-calibration');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/basic-line-graphs/disable-calibration');
       Terra.validates.screenshot('disable_calibration', { selector: '.carbon-graph-container' });
     });
 
     it('validates shapes hidden graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/line-graph/basic-line-graphs/shapes-hidden');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/basic-line-graphs/shapes-hidden');
       Terra.validates.screenshot('shapes_hidden', { selector: '.carbon-graph-container' });
     });
 
     it('validates show or hide shapes per dataset graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/line-graph/basic-line-graphs/show-or-hide-shapes-per-dataset');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/basic-line-graphs/show-or-hide-shapes-per-dataset');
       Terra.validates.screenshot('show_or_hide_shapes_per_dataset', { selector: '.carbon-graph-container' });
     });
 
     it('validates simple line graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/line-graph/basic-line-graphs/simple-line');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/basic-line-graphs/simple-line');
       Terra.validates.screenshot('simple_line', { selector: '.carbon-graph-container' });
     });
 
     it('validates timeseries graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/line-graph/basic-line-graphs/timeseries');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/basic-line-graphs/timeseries');
       Terra.validates.screenshot('timeseries', { selector: '.carbon-graph-container' });
     });
 
     it('validates timeseries with dateline graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/line-graph/basic-line-graphs/timeseries-with-dateline');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/basic-line-graphs/timeseries-with-dateline');
       Terra.validates.screenshot('timeseries_with_dateline', { selector: '.carbon-graph-container' });
     });
 
     it('validates timeseries with eventline graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/line-graph/basic-line-graphs/timeseries-with-eventline');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/basic-line-graphs/timeseries-with-eventline');
       Terra.validates.screenshot('timeseries_with_eventline', { selector: '.carbon-graph-container' });
     });
 
     it('validates y2-axis graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/line-graph/basic-line-graphs/y-2-axis');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/basic-line-graphs/y-2-axis');
       Terra.validates.screenshot('y2-axis', { selector: '.carbon-graph-container' });
     });
   });
 
   describe('Criticaliy', () => {
     it('validates criticality multi line graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/line-graph/criticality/multi-line');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/criticality/multi-line');
       Terra.validates.screenshot('criticality_multi_line', { selector: '.carbon-graph-container' });
     });
 
     it('validates criticality simple line graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/line-graph/criticality/simple-line');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/criticality/simple-line');
       Terra.validates.screenshot('criticality_simple_line', { selector: '.carbon-graph-container' });
     });
   });
 
   describe('Multi Line', () => {
     it('validates multi line graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/line-graph/multi-line/simple');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/multi-line/simple');
       browser.pause(750 * 6);
       Terra.validates.screenshot('multi_line ', { selector: '.carbon-graph-container' });
     });
@@ -68,7 +68,7 @@ Terra.describeViewports('LineGraph ', ['tiny', 'medium', 'large'], () => {
 
   describe('Non-contiguous', () => {
     it('validates non contiguous simple graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/line-graph/non-contiguous-line/simple');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/non-contiguous-line/simple');
       Terra.validates.screenshot('simple_non_contiguous', { selector: '.carbon-graph-container' });
     });
   });
@@ -76,44 +76,44 @@ Terra.describeViewports('LineGraph ', ['tiny', 'medium', 'large'], () => {
   describe('Regions', () => {
     describe('Line', () => {
       it('validates multiple regions graph container', () => {
-        browser.url('/raw/tests/terra-graphs/graphs/line-graph/regions/line/multiple-regions');
+        browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/regions/line/multiple-regions');
         Terra.validates.screenshot('multiple_regions', { selector: '.carbon-graph-container' });
       });
 
       it('validates region no lower bound graph container', () => {
-        browser.url('/raw/tests/terra-graphs/graphs/line-graph/regions/line/no-lower-bound');
+        browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/regions/line/no-lower-bound');
         Terra.validates.screenshot('region_no_lower-bound', { selector: '.carbon-graph-container' });
       });
 
       it('validates region no upper bound graph container', () => {
-        browser.url('/raw/tests/terra-graphs/graphs/line-graph/regions/line/no-upper-bound');
+        browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/regions/line/no-upper-bound');
         Terra.validates.screenshot('region_no_upper_bound', { selector: '.carbon-graph-container' });
       });
 
       it('validates regions line graph container', () => {
-        browser.url('/raw/tests/terra-graphs/graphs/line-graph/regions/line/regions-line');
+        browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/regions/line/regions-line');
         Terra.validates.screenshot('regions_line', { selector: '.carbon-graph-container' });
       });
 
       it('validates simple region graph container', () => {
-        browser.url('/raw/tests/terra-graphs/graphs/line-graph/regions/line/simple');
+        browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/regions/line/simple');
         Terra.validates.screenshot('simple_region', { selector: '.carbon-graph-container' });
       });
 
       it('validates value region graph container', () => {
-        browser.url('/raw/tests/terra-graphs/graphs/line-graph/regions/line/value-region');
+        browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/regions/line/value-region');
         Terra.validates.screenshot('value_region', { selector: '.carbon-graph-container' });
       });
       describe('Y2-Axis', () => {
-        beforeEach(() => browser.url('/raw/tests/terra-graphs/graphs/line-graph/regions/line/y-2-axis'));
+        beforeEach(() => browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/regions/line/y-2-axis'));
 
         it('validates y2-axis graph container', () => {
-          browser.url('/raw/tests/terra-graphs/graphs/line-graph/regions/line/y-2-axis');
+          browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/regions/line/y-2-axis');
           Terra.validates.screenshot('region_y2-axis', { selector: '.carbon-graph-container' });
         });
 
         it('validates y2-axis graph container when hovered on legend', () => {
-          browser.url('/raw/tests/terra-graphs/graphs/line-graph/regions/line/y-2-axis');
+          browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/regions/line/y-2-axis');
           Terra.validates.screenshot('region_y2-axis_when_hovered_on_legend', { selector: '.carbon-graph-container' });
         });
       });
@@ -121,19 +121,19 @@ Terra.describeViewports('LineGraph ', ['tiny', 'medium', 'large'], () => {
 
     describe('Multi line', () => {
       it('validates dataset region identical graph container', () => {
-        browser.url('/raw/tests/terra-graphs/graphs/line-graph/regions/multi-line/dataset-region-identical');
+        browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/regions/multi-line/dataset-region-identical');
         Terra.validates.screenshot('multiline_dataset_region_identical', { selector: '.carbon-graph-container' });
       });
 
       it('validates multiple line region graph container', () => {
-        browser.url('/raw/tests/terra-graphs/graphs/line-graph/regions/multi-line/multi-line');
+        browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/regions/multi-line/multi-line');
         Terra.validates.screenshot('multiple_line_region', { selector: '.carbon-graph-container' });
       });
 
       describe('when hovered on multiple line region legends', () => {
         let element = null;
         beforeEach(() => {
-          browser.url('/raw/tests/terra-graphs/graphs/line-graph/regions/multi-line/multi-line');
+          browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/regions/multi-line/multi-line');
           element = $('.carbon-legend').$$('li');
         });
         it('validates graph container when hovered on data label 1', () => {
@@ -154,7 +154,7 @@ Terra.describeViewports('LineGraph ', ['tiny', 'medium', 'large'], () => {
 
     describe('Spline', () => {
       it('validates spline region graph container', () => {
-        browser.url('/raw/tests/terra-graphs/graphs/line-graph/regions/spline/value-region');
+        browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/regions/spline/value-region');
         Terra.validates.screenshot('value_region', { selector: '.carbon-graph-container' });
       });
     });
@@ -162,7 +162,7 @@ Terra.describeViewports('LineGraph ', ['tiny', 'medium', 'large'], () => {
 
   describe('Spline', () => {
     it('validates simple spline graph container', () => {
-      browser.url('/raw/tests/terra-graphs/graphs/line-graph/spline/simple-spline');
+      browser.url('/raw/tests/terra-graphs-docs/graphs/line-graph/spline/simple-spline');
       Terra.validates.screenshot('simple_spline', { selector: '.carbon-graph-container' });
     });
   });
