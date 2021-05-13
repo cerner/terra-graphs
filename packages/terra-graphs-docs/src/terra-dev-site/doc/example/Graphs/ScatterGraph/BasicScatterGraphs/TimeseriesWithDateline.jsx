15bf63c97aa3dfc5b3272878802163a638905452
AshishMotanamGurunadham <motanamashish@gmail.com>
2021-05-13 12:26:12 -0500

    Added Examples

---
+++ packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs/ScatterGraph/BasicScatterGraphs/TimeseriesWithDateline.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import ScatterGraph from '@cerner/terra-graphs/src/components/Scatter/ScatterGraph';
+import Carbon from '@cerner/carbon-graphs/lib/js/carbon';
+import utils from '@cerner/carbon-graphs/lib/js/helpers/utils';
+import '@cerner/terra-graphs-docs/lib/Css/ExampleGraphContainer.module.scss';
+import lineTimesries from '@cerner/terra-graphs-docs/lib/example-datasets/graphConfigObjects/Line/lineTimeseries';
+import data from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Line/timeseriesData';
+import {
+  loadDatelinePopup,
+} from '@cerner/terra-graphs-docs/lib/example-datasets/popup';
+
+/*
+Please refer documentation below to see graphConfig and data objects.
+*/
+const graphConfig = utils.deepClone(lineTimesries('#timeseriesDateLine'));
+graphConfig.dateline = [
+  {
+    showDatelineIndicator: true,
+    label: {
+      display: 'Current Date',
+    },
+    color: '#C97318',
+    shape: Carbon.helpers.SHAPES.DARK.SQUARE,
+    onClick: loadDatelinePopup,
+    value: new Date(2016, 0, 1, 6).toISOString(),
+  },
+];
+graphConfig.clickPassThrough = {
+  dateline: false,
+};
+const dataset = utils.deepClone(data);
+
+export default () => (
+  <React.Fragment>
+    <div id="tooltip" className="initial-tooltip" />
+    <ScatterGraph graphID="timeseriesDateLine" graphConfig={graphConfig} dataset={dataset} />
+  </React.Fragment>
+);
---
+++ packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs/ScatterGraph/Criticality/Simple.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ScatterGraph from '@cerner/terra-graphs/src/components/Scatter/ScatterGraph';
+import utils from '@cerner/carbon-graphs/lib/js/helpers/utils';
+import '@cerner/terra-graphs-docs/lib/Css/ExampleGraphContainer.module.scss';
+import lineDefault from '@cerner/terra-graphs-docs/lib/example-datasets/graphConfigObjects/Line/lineDefault';
+import data from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Line/simpleLineData';
+
+/*
+Please refer documentation below to see graphConfig and data objects.
+*/
+const graphConfig = utils.deepClone(lineDefault('#simpleScattergraph'));
+
+const dataContent = utils.deepClone(data);
+dataContent[0].values[0].isCritical = true;
+dataContent[0].values[5].isCritical = true;
+dataContent[0].values[10].isCritical = true;
+export default () => (
+  <React.Fragment>
+    <div id="tooltip" className="initial-tooltip" />
+    <ScatterGraph graphID="simpleScattergraph" graphConfig={graphConfig} dataset={dataContent} />
+  </React.Fragment>
+);
