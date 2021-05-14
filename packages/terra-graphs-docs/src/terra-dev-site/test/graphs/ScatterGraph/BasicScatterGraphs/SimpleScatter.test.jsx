2526c92fca27a6068c664d73871c784fb2a6009b
AshishMotanamGurunadham <motanamashish@gmail.com>
2021-05-13 22:13:16 -0500

    Build Test 6

--- packages/terra-graphs-docs/src/terra-dev-site/test/graphs/ScatterGraph/BasicScatterGraphs/SimpleScatter.test.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ScatterGraph from '@cerner/terra-graphs/src/components/Scatter/ScatterGraph';
-import utils from '@cerner/carbon-graphs/lib/js/helpers/utils';
-import '@cerner/terra-graphs-docs/lib/Css/ExampleGraphContainer.module.scss';
-import lineDefault from '@cerner/terra-graphs-docs/lib/example-datasets/graphConfigObjects/Line/lineDefault';
-import data from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Line/simpleLineData';
