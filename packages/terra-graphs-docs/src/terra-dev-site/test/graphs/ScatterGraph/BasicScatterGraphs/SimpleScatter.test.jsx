2dd331efdccfbcb70c0687c38b593d00c3a0b246
AshishMotanamGurunadham <motanamashish@gmail.com>
2021-05-14 15:48:56 -0500

    Added four lines to test example

--- packages/terra-graphs-docs/src/terra-dev-site/test/graphs/ScatterGraph/BasicScatterGraphs/SimpleScatter.test.jsx
+++ packages/terra-graphs-docs/src/terra-dev-site/test/graphs/ScatterGraph/BasicScatterGraphs/SimpleScatter.test.jsx
@@ -1,3 +1,5 @@
 import React from 'react';
 import ScatterGraph from '@cerner/terra-graphs/src/components/Scatter/ScatterGraph';
+import utils from '@cerner/carbon-graphs/lib/js/helpers/utils';
+import '@cerner/terra-graphs-docs/lib/Css/ExampleGraphContainer.module.scss';
 
