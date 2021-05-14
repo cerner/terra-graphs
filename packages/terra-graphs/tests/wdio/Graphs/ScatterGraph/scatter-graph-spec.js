826ee82477c75d8f1e69ce686a368dec831d1251
AshishMotanamGurunadham <motanamashish@gmail.com>
2021-05-14 14:52:57 -0500

    Build test 13

--- packages/terra-graphs/tests/wdio/Graphs/ScatterGraph/scatter-graph-spec.js
+++
@@ -1,8 +0,0 @@
-Terra.describeViewports('ScatterGraph ', ['tiny', 'medium', 'large'], () => {
-  describe('Basic Satter graphs', () => {
-    it('validates simple scatter graph container', () => {
-      browser.url('/raw/tests/terra-graphs-docs/graphs/scatter-graph/basic-scatter-graphs/simple-scatter');
-      Terra.validates.screenshot('simple_scatter', { selector: '.carbon-graph-container' });
-    });
-  });
-});
