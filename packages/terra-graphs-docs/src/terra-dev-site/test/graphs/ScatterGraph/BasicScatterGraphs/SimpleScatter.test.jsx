858e1ac530752e8b3f7eea890ca1132662ac7ad3
AshishMotanamGurunadham <motanamashish@gmail.com>
2021-05-14 09:44:13 -0500

    Build Test 11

---
+++ packages/terra-graphs-docs/src/terra-dev-site/test/graphs/ScatterGraph/BasicScatterGraphs/SimpleScatter.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ScatterGraph from '@cerner/terra-graphs/src/components/Scatter/ScatterGraph';
+import utils from '@cerner/carbon-graphs/lib/js/helpers/utils';
+import '@cerner/terra-graphs-docs/lib/Css/ExampleGraphContainer.module.scss';
+import lineDefault from '@cerner/terra-graphs-docs/lib/example-datasets/graphConfigObjects/Line/lineDefault';
+import data from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Line/simpleLineData';
+
+/*
+Please refer to the documentation below to see the graphConfig and data objects.
+*/
+const graphConfig = utils.deepClone(lineDefault('#simpleScattergraph'));
+const dataset = utils.deepClone(data);
+
+export default () => (
+  <React.Fragment>
+    <div id="tooltip" className="initial-tooltip" />
+    <ScatterGraph graphID="simpleScattergraph" graphConfig={graphConfig} dataset={dataset} />
+  </React.Fragment>
+);
