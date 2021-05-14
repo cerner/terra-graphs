ee8c79334d224a14839cf0755b926c139840a8e3
AshishMotanamGurunadham <motanamashish@gmail.com>
2021-05-14 16:14:19 -0500

    Removed one line from test example import

--- packages/terra-graphs-docs/src/terra-dev-site/test/graphs/ScatterGraph/BasicScatterGraphs/SimpleScatter.test.jsx
+++ packages/terra-graphs-docs/src/terra-dev-site/test/graphs/ScatterGraph/BasicScatterGraphs/SimpleScatter.test.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
 import ScatterGraph from '@cerner/terra-graphs/src/components/Scatter/ScatterGraph';
 import utils from '@cerner/carbon-graphs/lib/js/helpers/utils';
-import '@cerner/terra-graphs-docs/lib/Css/ExampleGraphContainer.module.scss';
 
