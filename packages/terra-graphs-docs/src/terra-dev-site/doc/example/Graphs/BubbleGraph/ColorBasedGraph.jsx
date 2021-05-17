4b40fb5cb0cabecb4192d706b5b807166ea9ed01
Saad <saadadnan94@gmail.com>
2021-05-17 11:47:42 -0500

    updated import

--- packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs/BubbleGraph/ColorBasedGraph.jsx
+++ packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs/BubbleGraph/ColorBasedGraph.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import BubbleSingleDataset from '@cerner/terra-graphs/lib/components/Bubble/BubbleSingleDataset';
 import utils from '@cerner/carbon-graphs/lib/js/helpers/utils';
 import '@cerner/terra-graphs/lib//components/Graph.module.scss';
-import '@cerner/terra-graphs/lib//components/Dev.module.scss';
+import '@cerner/terra-graphs-docs/lib/Css/ExampleGraphContainer.module.scss';
 import getSimpleAxisData from '@cerner/terra-graphs-docs/src/example-datasets/graphConfigObjects/Bubble/simpleAxisData';
 import bubbleDataColorBased from '@cerner/terra-graphs-docs/src/example-datasets/dataObjects/Bubble/bubbleDataColorBased.js';
 
