c1adcad35276428957cf4f101860e7250c680da2
AshishMotanamGurunadham <63670637+AshishMotanamGurunadham@users.noreply.github.com>
2021-05-18 14:49:52 -0500

    Created component, examples and WDIO tests for scatter graph (#120)
    * Created component, examples and WDIO tests for scatter graph
    
    * Changes made to correct build
    
    * Made changes to popup.js file
    
    * Made changes to example imports

---
+++ packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs/ScatterGraph/BasicScatterGraphs/TimeseriesWithEventline.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ScatterGraph from '@cerner/terra-graphs/lib/components/Scatter/ScatterGraph';
+import Carbon from '@cerner/carbon-graphs/lib/js/carbon';
+import utils from '@cerner/carbon-graphs/lib/js/helpers/utils';
+import '@cerner/terra-graphs-docs/lib/Css/ExampleGraphContainer.module.scss';
+import lineTimesries from '@cerner/terra-graphs-docs/lib/example-datasets/graphConfigObjects/Line/lineTimeseries';
+import data from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Line/timeseriesData';
+
+/*
+Please refer documentation below to see graphConfig and data objects.
+*/
+const graphConfig = utils.deepClone(lineTimesries('#timeseriesEventLine'));
+graphConfig.eventline = [
+  {
+    color: Carbon.helpers.COLORS.GREY,
+    style: {
+      strokeDashArray: '4,4',
+    },
+    value: new Date(2016, 0, 1, 8).toISOString(),
+  },
+  {
+    color: Carbon.helpers.COLORS.BLACK,
+    style: {
+      strokeDashArray: '2,2',
+    },
+    value: new Date(2016, 0, 1, 12).toISOString(),
+  },
+];
+const dataset = utils.deepClone(data);
+
+export default () => (
+  <React.Fragment>
+    <div id="tooltip" className="initial-tooltip" />
+    <ScatterGraph graphID="timeseriesEventLine" graphConfig={graphConfig} dataset={dataset} />
+  </React.Fragment>
+);
---
+++ packages/terra-graphs-docs/src/terra-dev-site/test/graphs/ScatterGraph/BasicScatterGraphs/Y2Axis.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ScatterGraph from '@cerner/terra-graphs/lib/components/Scatter/ScatterGraph';
+import utils from '@cerner/carbon-graphs/lib/js/helpers/utils';
+import '@cerner/terra-graphs-docs/lib/Css/ExampleGraphContainer.module.scss';
+import lineTimesries from '@cerner/terra-graphs-docs/lib/example-datasets/graphConfigObjects/Line/lineTimeseries';
+import data from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Line/y2AxisData';
+/*
+Please refer to the documentation below to see the graphConfig and data objects.
+*/
+const graphConfig = utils.deepClone(lineTimesries('#scatterY2Axis'));
+graphConfig.axis.y2.show = true;
+const dataset = utils.deepClone(data);
+
+export default () => (
+  <React.Fragment>
+    <div id="tooltip" className="initial-tooltip" />
+    <ScatterGraph graphID="scatterY2Axis" graphConfig={graphConfig} dataset={dataset} />
+  </React.Fragment>
+);
