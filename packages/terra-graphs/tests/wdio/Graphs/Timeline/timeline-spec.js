c6487654db96534e6928b9bc2dabea8782126abe
Saad <saadadnan94@gmail.com>
2021-05-21 19:40:31 -0500

    updated tests

--- packages/terra-graphs/tests/wdio/Graphs/Timeline/timeline-spec.js
+++ packages/terra-graphs/tests/wdio/Graphs/Timeline/timeline-spec.js
@@ -2,7 +2,7 @@ Terra.describeViewports('Timeline graphs', ['tiny', 'medium', 'large'], () => {
     describe('Validates', () => {
       it('basic timeline graph container', () => {
         browser.url('/raw/tests/terra-graphs-docs/graphs/timeline/basic-timeline');
-        Terra.validates.screenshot('single-dataset', { selector: '.carbon-timeline-container' });
+        Terra.validates.screenshot('simple-timeline', { selector: '.carbon-timeline-container' });
       });
     });
   });
