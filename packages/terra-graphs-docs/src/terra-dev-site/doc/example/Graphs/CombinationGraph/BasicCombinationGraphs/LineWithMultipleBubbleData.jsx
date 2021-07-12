786217520972f01ebb626da4944bc968b5c6db71
AshishMotanamGurunadham <motanamashish@gmail.com>
2021-07-12 15:00:01 -0500

    Made changes to react example folder

---
+++ packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs/CombinationGraph/BasicCombinationGraphs/LineWithMultipleBubbleData.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import CombinationGraph from '@cerner/terra-graphs/lib/components/Combination/CombinationGraph';
+import utils from '@cerner/carbon-graphs/lib/js/helpers/utils';
+import '@cerner/terra-graphs-docs/lib/Css/ExampleGraphContainer.module.scss';
+import getCombinationGraphConfig from '@cerner/terra-graphs-docs/lib/example-datasets/graphConfigObjects/Combination/combinationDefault';
+import data from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Combination/lineWithMultipleBubbleData';
+
+/*
+Please refer to the documentation below to see the graphConfig and data objects
+*/
+const graphConfig = utils.deepClone(getCombinationGraphConfig('#lineWithMultipleBubbleData'));
+const dataset = utils.deepClone(data);
+
+export default () => (
+  <React.Fragment>
+    <div id="tooltip" className="initial-tooltip" />
+    <CombinationGraph graphID="lineWithMultipleBubbleData" graphConfig={graphConfig} dataset={dataset} />
+  </React.Fragment>
+);
