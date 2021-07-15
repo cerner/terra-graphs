ba24d53fdf517a706c6da7c2251a50ee4717a62e
Saad <saadadnan94@gmail.com>
2021-07-15 18:20:32 -0500

    updated basic bar graph examples

--- packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs-Carbon/BarGraph/BasicBarGraphs/Timeseries.jsx
+++ packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs-Carbon/BarGraph/BasicBarGraphs/Timeseries.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
-import BarGraph from '@cerner/terra-graphs/lib/components/Bar/BarGraph';
+import Carbon from '@cerner/carbon-graphs/lib/js/carbon';
 import utils from '@cerner/carbon-graphs/lib/js/helpers/utils';
 import '@cerner/terra-graphs-docs/lib/Css/ExampleGraphContainer.module.scss';
+import '@cerner/terra-graphs/lib/components/Graph.module.scss';
+import '@cerner/terra-graphs/src/components/Bar/BarGraph.module.scss';
 import barTimeseries from '@cerner/terra-graphs-docs/lib/example-datasets/graphConfigObjects/Bar/barTimeseries';
 import data from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Bar/timeseriesData';
 
 /*
 Please refer to the documentation below to see the graphConfig and data objects
 */
+
 const graphConfig = utils.deepClone(barTimeseries('#timeseriesBargraph'));
+graphConfig.axis.y.lowerLimit = -15;
+graphConfig.axis.y.upperLimit = 0;
 const dataset = utils.deepClone(data);
 
-export default () => (
-  <React.Fragment>
-    <div id="tooltip" className="initial-tooltip" />
-    <BarGraph graphID="timeseriesBargraph" graphConfig={graphConfig} dataset={dataset} />
-  </React.Fragment>
-);
+export default () => {
+  React.useEffect(() => {
+    const graph = Carbon.api.graph(graphConfig);
+    graph.loadContent(Carbon.api.bar(dataset));
+  }, []);
+
+  return (
+    <React.Fragment>
+      <div id="tooltip" className="initial-tooltip" />
+      <div id="timeseriesBargraph"> </div>
+    </React.Fragment>
+  );
+};
+
--- packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs-Carbon/BarGraph/BasicBarGraphs/TimeseriesWithEventline.jsx
+++ packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs-Carbon/BarGraph/BasicBarGraphs/TimeseriesWithEventline.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Carbon from '@cerner/carbon-graphs/lib/js/carbon';
-import BarGraph from '@cerner/terra-graphs/lib/components/Bar/BarGraph';
 import utils from '@cerner/carbon-graphs/lib/js/helpers/utils';
 import '@cerner/terra-graphs-docs/lib/Css/ExampleGraphContainer.module.scss';
+import '@cerner/terra-graphs/lib/components/Graph.module.scss';
+import '@cerner/terra-graphs/src/components/Bar/BarGraph.module.scss';
 import barTimeseries from '@cerner/terra-graphs-docs/lib/example-datasets/graphConfigObjects/Bar/barTimeseries';
 import data from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Bar/timeseriesData';
 
 /*
 Please refer to the documentation below to see the graphConfig and data objects
 */
+
 const graphConfig = utils.deepClone(barTimeseries('#timeseriesBarWithEventline'));
 
 graphConfig.eventline = [
@@ -33,9 +35,17 @@ graphConfig.clickPassThrough = {
 graphConfig.showVGrid = false;
 const dataset = utils.deepClone(data);
 
-export default () => (
-  <React.Fragment>
-    <div id="tooltip" className="initial-tooltip" />
-    <BarGraph graphID="timeseriesBarWithEventline" graphConfig={graphConfig} dataset={dataset} />
-  </React.Fragment>
-);
+export default () => {
+  React.useEffect(() => {
+    const graph = Carbon.api.graph(graphConfig);
+    graph.loadContent(Carbon.api.bar(dataset));
+  }, []);
+
+  return (
+    <React.Fragment>
+      <div id="tooltip" className="initial-tooltip" />
+      <div id="timeseriesBarWithDateline"> </div>
+    </React.Fragment>
+  );
+};
+
