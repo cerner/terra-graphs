58ca33b33e2f629d9782e655ed873f6051c891c4
Saad <saadadnan94@gmail.com>
2021-07-15 18:20:32 -0500

    updated basic bar graph examples

--- packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs-Carbon/BarGraph/BasicBarGraphs/NegativeBars.jsx
+++ packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs-Carbon/BarGraph/BasicBarGraphs/NegativeBars.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
-import BarGraph from '@cerner/terra-graphs/lib/components/Bar/BarGraph';
+import Carbon from '@cerner/carbon-graphs/lib/js/carbon';
 import utils from '@cerner/carbon-graphs/lib/js/helpers/utils';
 import '@cerner/terra-graphs-docs/lib/Css/ExampleGraphContainer.module.scss';
+import '@cerner/terra-graphs/lib/components/Graph.module.scss';
+import '@cerner/terra-graphs/src/components/Bar/BarGraph.module.scss';
 import barDefault from '@cerner/terra-graphs-docs/lib/example-datasets/graphConfigObjects/Bar/barDefault';
 import data from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Bar/negativeBarData';
 
 /*
 Please refer to the documentation below to see the graphConfig and data objects
 */
+
 const graphConfig = utils.deepClone(barDefault('#negativeBargraph'));
 graphConfig.axis.y.lowerLimit = -15;
 graphConfig.axis.y.upperLimit = 0;
 const dataset = utils.deepClone(data);
 
-export default () => (
-  <React.Fragment>
-    <div id="tooltip" className="initial-tooltip" />
-    <BarGraph graphID="negativeBargraph" graphConfig={graphConfig} dataset={dataset} />
-  </React.Fragment>
-);
+export default () => {
+  React.useEffect(() => {
+    const graph = Carbon.api.graph(graphConfig);
+    graph.loadContent(Carbon.api.bar(dataset));
+  }, []);
+
+  return (
+    <React.Fragment>
+      <div id="tooltip" className="initial-tooltip" />
+      <div id="negativeBargraph"> </div>
+    </React.Fragment>
+  );
+};
--- packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs-Carbon/BarGraph/BasicBarGraphs/StackedAxisInfoTextLabels.jsx
+++ packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs-Carbon/BarGraph/BasicBarGraphs/StackedAxisInfoTextLabels.jsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import Carbon from '@cerner/carbon-graphs/lib/js/carbon';
-import BarGraph from '@cerner/terra-graphs/lib/components/Bar/BarGraph';
 import utils from '@cerner/carbon-graphs/lib/js/helpers/utils';
 import '@cerner/terra-graphs-docs/lib/Css/ExampleGraphContainer.module.scss';
+import '@cerner/terra-graphs/lib/components/Graph.module.scss';
+import '@cerner/terra-graphs/src/components/Bar/BarGraph.module.scss';
 import barDefault from '@cerner/terra-graphs-docs/lib/example-datasets/graphConfigObjects/Bar/barDefault';
 import data from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Bar/stackedAxisInfoTextLabelsData';
 
 /*
 Please refer to the documentation below to see the graphConfig and data objects
 */
+
 const graphConfig = utils.deepClone(barDefault('#stackedAxisInfoTextLabelsBargraph'));
-graphConfig.axis.x.orientation = Carbon.helpers.AXES_ORIENTATION.X.TOP;
+graphConfig.axis.y.lowerLimit = -15;
+graphConfig.axis.y.upperLimit = 0;
+const dataset = utils.deepClone(data);
 
-const contentData = utils.deepClone(data);
-contentData[0].label.isDisabled = true;
-contentData[1].label.isDisabled = true;
-contentData[2].label.isDisabled = true;
+export default () => {
+  React.useEffect(() => {
+    const graph = Carbon.api.graph(graphConfig);
+    graph.loadContent(Carbon.api.bar(dataset));
+  }, []);
 
-export default () => (
-  <React.Fragment>
-    <div id="tooltip" className="initial-tooltip" />
-    <BarGraph graphID="stackedAxisInfoTextLabelsBargraph" graphConfig={graphConfig} dataset={contentData} />
-  </React.Fragment>
-);
+  return (
+    <React.Fragment>
+      <div id="tooltip" className="initial-tooltip" />
+      <div id="stackedAxisInfoTextLabelsBargraph"> </div>
+    </React.Fragment>
+  );
+};
+
