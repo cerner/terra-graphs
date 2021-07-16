629502f50ba64ee65fc7d97f505f29937e6313ac
AshishMotanamGurunadham <63670637+AshishMotanamGurunadham@users.noreply.github.com>
2021-07-15 23:44:04 -0500

    Updated documentation for General graphs to be carbon centric. (#184)

---
+++ packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs-Carbon/General/Other/BackgroundColor.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import utils from '@cerner/carbon-graphs/lib/js/helpers/utils';
+import Carbon from '@cerner/carbon-graphs/lib/js/carbon';
+import '@cerner/terra-graphs-docs/lib/Css/ExampleGraphContainer.module.scss';
+import '@cerner/terra-graphs/lib/components/Graph.module.scss';
+import '@cerner/terra-graphs/lib/components/Line/LineGraph.module.scss';
+import lineDefault from '@cerner/terra-graphs-docs/lib/example-datasets/graphConfigObjects/Line/lineDefault';
+import exampleData from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Line/simpleLineData';
+
+/*
+Please refer to the documentation below to see the graphConfig and data objects
+*/
+const graphConfig = utils.deepClone(lineDefault('#backgroundColor'));
+const dataset = utils.deepClone(exampleData);
+
+const BackgroundColorGeneralExample = () => {
+  React.useEffect(() => {
+    const graph = Carbon.api.graph(graphConfig);
+    dataset.forEach((data) => {
+      graph.loadContent(Carbon.api.line(data));
+    });
+  }, []);
+  return (
+    <React.Fragment>
+      <div id="tooltip" className="initial-tooltip" />
+      <div id="backgroundColor" />
+    </React.Fragment>
+  );
+};
+
+export default BackgroundColorGeneralExample;
