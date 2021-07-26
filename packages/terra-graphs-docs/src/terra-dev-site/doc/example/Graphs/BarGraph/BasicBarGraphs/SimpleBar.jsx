76120c83a37b69f7139ab9f61d26ce86ca0d8dd4
Saad <saadadnan94@gmail.com>
2021-07-26 14:58:58 -0500

    working examples

--- packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs/BarGraph/BasicBarGraphs/SimpleBar.jsx
+++ packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs/BarGraph/BasicBarGraphs/SimpleBar.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
 import BarGraph from '@cerner/terra-graphs/lib/components/Bar/BarGraph';
-import utils from '@cerner/carbon-graphs/lib/js/helpers/utils';
-import '@cerner/terra-graphs-docs/lib/Css/ExampleGraphContainer.module.scss';
-import barDefault from '@cerner/terra-graphs-docs/lib/example-datasets/graphConfigObjects/Bar/barDefault';
-import data from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Bar/simpleBarData';
+ import utils from '@cerner/carbon-graphs/lib/js/helpers/utils';
+ import '@cerner/terra-graphs-docs/lib/Css/ExampleGraphContainer.module.scss';
+ import Carbon from '@cerner/carbon-graphs/lib/js/carbon';
+ import '@cerner/terra-graphs/lib/components/Graph.module.scss';
+ import '@cerner/terra-graphs/lib/components/Bar/BarGraph.module.scss';
+ import barDefault from '@cerner/terra-graphs-docs/lib/example-datasets/graphConfigObjects/Bar/barDefault';
+ import exampleData from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Bar/simpleBarData';
 
-/*
-Please refer to the documentation below to see the graphConfig and data objects
-*/
-const graphConfig = utils.deepClone(barDefault('#simpleBargraph'));
-const dataset = utils.deepClone(data);
+ /*
+ Please refer to the documentation below to see the graphConfig and data objects
+ */
+ const graphConfig = utils.deepClone(barDefault('#simpleBargraph'));
+ const dataset = utils.deepClone(exampleData);
 
-export default () => (
-  <React.Fragment>
-    <div id="tooltip" className="initial-tooltip" />
-    <BarGraph graphID="simpleBargraph" graphConfig={graphConfig} dataset={dataset} />
-  </React.Fragment>
-);
+export default () => {
+   React.useEffect(() => {
+     const graph = Carbon.api.graph(graphConfig);
+     dataset.forEach((data) => {
+       graph.loadContent(Carbon.api.bar(data));
+     });
+   }, []);
+   return (
+     <React.Fragment>
+       <div id="tooltip" className="initial-tooltip" />
+       <div id="simpleBargraph" />
+     </React.Fragment>
+   );
+ };
+
