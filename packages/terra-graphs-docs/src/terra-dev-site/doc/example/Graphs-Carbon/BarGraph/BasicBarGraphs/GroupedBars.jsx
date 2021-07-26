973b141976197f54c2e56683db1f8a6a9ad060ec
Saad <saadadnan94@gmail.com>
2021-07-26 11:42:14 -0500

    executed linter

--- packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs-Carbon/BarGraph/BasicBarGraphs/GroupedBars.jsx
+++ packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs-Carbon/BarGraph/BasicBarGraphs/GroupedBars.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
- import Carbon from '@cerner/carbon-graphs/lib/js/carbon';
- import utils from '@cerner/carbon-graphs/lib/js/helpers/utils';
- import '@cerner/terra-graphs-docs/lib/Css/ExampleGraphContainer.module.scss';
- import '@cerner/terra-graphs/lib/components/Graph.module.scss';
- import '@cerner/terra-graphs/src/components/Bar/BarGraph.module.scss';
- import barDefault from '@cerner/terra-graphs-docs/lib/example-datasets/graphConfigObjects/Bar/barDefault';
- import exampleData from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Bar/groupedBarData';
+import Carbon from '@cerner/carbon-graphs/lib/js/carbon';
+import utils from '@cerner/carbon-graphs/lib/js/helpers/utils';
+import '@cerner/terra-graphs-docs/lib/Css/ExampleGraphContainer.module.scss';
+import '@cerner/terra-graphs/lib/components/Graph.module.scss';
+import '@cerner/terra-graphs/src/components/Bar/BarGraph.module.scss';
+import barDefault from '@cerner/terra-graphs-docs/lib/example-datasets/graphConfigObjects/Bar/barDefault';
+import exampleData from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Bar/groupedBarData';
 
- /*
+/*
  Please refer to the documentation below to see the graphConfig and data objects
  */
 
- const graphConfig = utils.deepClone(barDefault('#groupedBarGraph'));
- const dataset = utils.deepClone(exampleData);
+const graphConfig = utils.deepClone(barDefault('#groupedBarGraph'));
+const dataset = utils.deepClone(exampleData);
 
- export default () => {
-   React.useEffect(() => {
-     const graph = Carbon.api.graph(graphConfig);
-     dataset.forEach((data) => {
-       graph.loadContent(Carbon.api.bar(data));
-     });
-   }, []);
+export default () => {
+  React.useEffect(() => {
+    const graph = Carbon.api.graph(graphConfig);
+    dataset.forEach((data) => {
+      graph.loadContent(Carbon.api.bar(data));
+    });
+  }, []);
 
-   return (
-     <React.Fragment>
-       <div id="tooltip" className="initial-tooltip" />
-       <div id="groupedBarGraph"> </div>
-     </React.Fragment>
-   );
- };
+  return (
+    <React.Fragment>
+      <div id="tooltip" className="initial-tooltip" />
+      <div id="groupedBarGraph"> </div>
+    </React.Fragment>
+  );
+};
--- packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs-Carbon/BarGraph/BasicBarGraphs/NegativeBars.jsx
+++ packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs-Carbon/BarGraph/BasicBarGraphs/NegativeBars.jsx
@@ -1,33 +1,33 @@
 import React from 'react';
- import Carbon from '@cerner/carbon-graphs/lib/js/carbon';
- import utils from '@cerner/carbon-graphs/lib/js/helpers/utils';
- import '@cerner/terra-graphs-docs/lib/Css/ExampleGraphContainer.module.scss';
- import '@cerner/terra-graphs/lib/components/Graph.module.scss';
- import '@cerner/terra-graphs/src/components/Bar/BarGraph.module.scss';
- import barDefault from '@cerner/terra-graphs-docs/lib/example-datasets/graphConfigObjects/Bar/barDefault';
- import exampleData from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Bar/negativeBarData';
+import Carbon from '@cerner/carbon-graphs/lib/js/carbon';
+import utils from '@cerner/carbon-graphs/lib/js/helpers/utils';
+import '@cerner/terra-graphs-docs/lib/Css/ExampleGraphContainer.module.scss';
+import '@cerner/terra-graphs/lib/components/Graph.module.scss';
+import '@cerner/terra-graphs/src/components/Bar/BarGraph.module.scss';
+import barDefault from '@cerner/terra-graphs-docs/lib/example-datasets/graphConfigObjects/Bar/barDefault';
+import exampleData from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Bar/negativeBarData';
 
- /*
+/*
  Please refer to the documentation below to see the graphConfig and data objects
  */
 
- const graphConfig = utils.deepClone(barDefault('#negativeBargraph'));
- graphConfig.axis.y.lowerLimit = -15;
- graphConfig.axis.y.upperLimit = 0;
- const dataset = utils.deepClone(exampleData);
+const graphConfig = utils.deepClone(barDefault('#negativeBargraph'));
+graphConfig.axis.y.lowerLimit = -15;
+graphConfig.axis.y.upperLimit = 0;
+const dataset = utils.deepClone(exampleData);
 
- export default () => {
-   React.useEffect(() => {
-     const graph = Carbon.api.graph(graphConfig);
-     dataset.forEach((data) => {
-       graph.loadContent(Carbon.api.bar(data));
-     });
-   }, []);
+export default () => {
+  React.useEffect(() => {
+    const graph = Carbon.api.graph(graphConfig);
+    dataset.forEach((data) => {
+      graph.loadContent(Carbon.api.bar(data));
+    });
+  }, []);
 
-   return (
-     <React.Fragment>
-       <div id="tooltip" className="initial-tooltip" />
-       <div id="negativeBargraph"> </div>
-     </React.Fragment>
-   );
- };
+  return (
+    <React.Fragment>
+      <div id="tooltip" className="initial-tooltip" />
+      <div id="negativeBargraph"> </div>
+    </React.Fragment>
+  );
+};
