5a7c2a968bdcb226cceafa887027b703e7b9fe45
AshishMotanamGurunadham <motanamashish@gmail.com>
2021-09-07 17:15:11 -0500

    Updated Timeline datasets and examples to maintain consistency.

--- packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs-Carbon/Timeline/Reflow/SimplePanning.jsx
+++ packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs-Carbon/Timeline/Reflow/SimplePanning.jsx
@@ -5,15 +5,15 @@ import Carbon from '@cerner/carbon-graphs/lib/js/carbon';
 import '@cerner/terra-graphs-docs/lib/Css/ExampleGraphContainer.module.scss';
 import '@cerner/terra-graphs/lib/components/Graph.module.scss';
 import '@cerner/terra-graphs/lib/components/Timeline/Timeline.module.scss';
-import exampleData from '@cerner/terra-graphs-docs/src/example-datasets/dataObjects/Timeline/defaultDataset';
 import getTimelinePanningConfig from '@cerner/terra-graphs-docs/lib/example-datasets/graphConfigObjects/Timeline/timelinePanningConfig';
+import dataset1 from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Timeline/defaultDataset1.js';
+import dataset2 from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Timeline/defaultDataset2.js';
 
 /*
 Please refer to the documentation below to see the graphConfig and data objects
 */
-
 const graphConfig = utils.deepClone(getTimelinePanningConfig('#TimelinePanningExample'));
-const dataset = [utils.deepClone(exampleData)];
+const dataset = [utils.deepClone(dataset1), utils.deepClone(dataset2)];
 
 const initialState = {
   initial: 0,
--- packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs/Timeline/BasicTimeline/AxisWithNoTickValues.jsx
+++ packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs/Timeline/BasicTimeline/AxisWithNoTickValues.jsx
@@ -2,17 +2,16 @@ import React from 'react';
 import Timeline from '@cerner/terra-graphs/lib/components/Timeline/Timeline';
 import utils from '@cerner/carbon-graphs/lib/js/helpers/utils';
 import '@cerner/terra-graphs-docs/lib/Css/ExampleGraphContainer.module.scss';
-import getTimelineConfig from '@cerner/terra-graphs-docs/lib/example-datasets/graphConfigObjects/Timeline/timelineConfigObject';
-import data from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Timeline/defaultDataset.js';
+import getTimelineConfig from '@cerner/terra-graphs-docs/lib/example-datasets/graphConfigObjects/Timeline/timelineWithNoAxisTicksConfig';
+import dataset1 from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Timeline/defaultDataset1.js';
+import dataset2 from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Timeline/defaultDataset2.js';
 
 /*
 Please refer to the documentation below to see the graphConfig and data objects
 */
 const graphConfig = utils.deepClone(getTimelineConfig('#simpleTimelineGraph'));
-const dataset = utils.deepClone(data);
-graphConfig.axis.x.ticks = {
-  format: '',
-};
+const dataset = [utils.deepClone(dataset1), utils.deepClone(dataset2)];
+
 export default () => (
   <React.Fragment>
     <div id="tooltip" className="initial-tooltip" />
