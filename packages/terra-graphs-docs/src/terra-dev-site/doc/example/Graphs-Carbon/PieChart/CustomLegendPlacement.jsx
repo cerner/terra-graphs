7c73361556bd18150f263a377ed4232c1c7d3496
Saad <saadadnan94@gmail.com>
2021-09-08 17:13:44 -0500

    updated imports for carbon scatter graph examples

--- packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs-Carbon/PieChart/CustomLegendPlacement.jsx
+++ packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs-Carbon/PieChart/CustomLegendPlacement.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import utils from '@cerner/carbon-graphs/lib/js/helpers/utils';
 import Carbon from '@cerner/carbon-graphs/lib/js/carbon';
-import '@cerner/terra-graphs-docs/lib/Css/ExampleGraphContainer.module.scss';
 import '@cerner/terra-graphs/lib/components/Graph.module.scss';
 import '@cerner/terra-graphs/lib/components/Pie/PieChart.module.scss';
+import '@cerner/terra-graphs-docs/lib/Css/ExampleGraphContainer.module.scss';
+import '@cerner/terra-graphs-docs/lib/Css/ExampleGraphContainer.module.scss';
 import getPieConfig from '@cerner/terra-graphs-docs/lib/example-datasets/graphConfigObjects/Pie/customLegendPlacementConfig';
 import dataset1 from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Pie/simplePieDataset1';
 import dataset2 from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Pie/simplePieDataset2';
