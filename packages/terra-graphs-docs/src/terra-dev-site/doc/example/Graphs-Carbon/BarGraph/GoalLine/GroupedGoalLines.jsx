6cb7c42d7540bb16e0bcbeb5e4d70a8ada4f611a
Saad <saadadnan94@gmail.com>
2021-09-16 15:28:43 -0500

    updated Bar GoalLine folder

--- packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs-Carbon/BarGraph/GoalLine/GroupedGoalLines.jsx
+++ packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs-Carbon/BarGraph/GoalLine/GroupedGoalLines.jsx
@@ -82,7 +82,7 @@ export default () => {
   return (
     <React.Fragment>
       <div id="tooltip" className="initial-tooltip" />
-      <div id="groupedGoalLineBargraph"> </div>
+      <div id="groupedGoalLineBargraph" class="example-graph-container"> </div>
     </React.Fragment>
   );
 };
--- packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs-Carbon/BarGraph/GoalLine/StackedGoalLines.jsx
+++ packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs-Carbon/BarGraph/GoalLine/StackedGoalLines.jsx
@@ -56,7 +56,7 @@ export default () => {
   return (
     <React.Fragment>
       <div id="tooltip" className="initial-tooltip" />
-      <div id="stackedGoalLineBargraph"> </div>
+      <div id="stackedGoalLineBargraph" class="example-graph-container"> </div>
     </React.Fragment>
   );
 };
