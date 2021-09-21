2e3c52492e8ef9a74b8aaa5bd3519cccbfa1ba08
Saad <saadadnan94@gmail.com>
2021-09-21 17:40:32 -0500

    updated scatter graph react examples

--- packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs/ScatterGraph/BasicScatterGraphs/Y2Axis.jsx
+++ packages/terra-graphs-docs/src/terra-dev-site/doc/example/Graphs/ScatterGraph/BasicScatterGraphs/Y2Axis.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import ScatterGraph from '@cerner/terra-graphs-docs/lib/terra-graphs-src/components/Scatter/ScatterGraph';
 import utils from '@cerner/carbon-graphs/lib/js/helpers/utils';
+import ScatterGraph from '@cerner/terra-graphs-docs/lib/terra-graphs-src/components/Scatter/ScatterGraph';
 import '@cerner/terra-graphs-docs/lib/terra-dev-site/ExampleGraphContainer/ExampleGraphContainer.module.scss';
 import lineTimesries from '@cerner/terra-graphs-docs/lib/example-datasets/graphConfigObjects/Line/lineTimeseries';
 import data from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Line/y2AxisData';
