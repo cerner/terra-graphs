02f965620c51627510df83c6bd18eadf8b5cd346
Dmytro Chumeiko <dc094404@cerner.net>
2022-11-07 16:47:57 +0200

    fixed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,46 @@
-const { merge } = require("webpack-merge");
-const { TerraDevSite } = require("@cerner/terra-dev-site");
-const path = require("path");
+const { merge } = require('webpack-merge');
+const {
+  TerraDevSite,
+} = require('@cerner/terra-dev-site');
+const path = require('path');
 
-const WebpackConfigTerra = require("@cerner/webpack-config-terra");
+const WebpackConfigTerra = require('@cerner/webpack-config-terra');
 
 const coreConfig = (env = {}) => ({
   plugins: [
     new TerraDevSite({
       defaultLocale: env.defaultLocale,
-      defaultTheme: "orion-fusion-theme",
-      primaryNavigationItems: [
-        {
-          path: "/home",
-          label: "Home",
-          contentExtension: "home",
-          additionalContent: [
-            {
-              label: "Home",
-              filePath: path.resolve(process.cwd(), "README.md"),
-            },
-          ],
-        },
-        {
-          path: "/graphs",
-          label: "Graphs",
-          contentExtension: "graph",
-        },
-        {
-          path: "/tests",
-          label: "Tests",
-          contentExtension: "test",
-        },
-      ],
+      defaultTheme: 'orion-fusion-theme',
+      primaryNavigationItems: [{
+        path: '/home',
+        label: 'Home',
+        contentExtension: 'home',
+        additionalContent: [
+          {
+            label: 'Home',
+            filePath: path.resolve(process.cwd(), 'README.md'),
+          },
+        ],
+      }, {
+        path: '/graphs',
+        label: 'Graphs',
+        contentExtension: 'graph',
+      }, {
+        path: '/tests',
+        label: 'Tests',
+        contentExtension: 'test',
+      }],
     }),
   ],
   resolve: {
     fallback: {
-      util: require.resolve("util/"),
+      util: require.resolve('util/'),
     },
   },
 });
 
-const mergedConfig = (env, argv) =>
-  merge(WebpackConfigTerra(env, argv), coreConfig());
+const mergedConfig = (env, argv) => (
+  merge(WebpackConfigTerra(env, argv), coreConfig())
+);
 
 module.exports = mergedConfig;
