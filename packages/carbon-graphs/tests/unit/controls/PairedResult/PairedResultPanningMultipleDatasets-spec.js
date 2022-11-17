211a78b62394aab299678c437ba228bc3522788a
Dmytro Chumeiko <dmitryi.chumeiko@gmail.com>
2022-11-17 17:05:55 +0200

    UXPLATFORM-6962 lint fixed

--- packages/carbon-graphs/tests/unit/controls/PairedResult/PairedResultPanningMultipleDatasets-spec.js
+++ packages/carbon-graphs/tests/unit/controls/PairedResult/PairedResultPanningMultipleDatasets-spec.js
@@ -1094,7 +1094,7 @@ describe('PairedResult', () => {
           done();
         };
 
-        triggerLegendItems();       
+        triggerLegendItems();
 
         graphDefault.reflowMultipleDatasets(graphData);
         graphDefault.reflowMultipleDatasets(graphData1);
