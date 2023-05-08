4d732af71b798076bf8b8308fb3ea47581d76c86
Saad Adnan <Saad.Adnan@cerner.com>
2023-05-08 14:07:10 -0500

    moved wdio tests to carbon-graphs package

--- packages/terra-graphs/tests/wdio/graphs-spec.js
+++
@@ -1,9 +0,0 @@
-// Terra.describeViewports('Graphs', ['tiny', 'medium', 'large'], () => {
-//   describe('Default', () => {
-//     before(() => browser.url('/raw/tests/terra-graphs/docs/default-graphs'));
-
-//     it('validates the element', () => {
-//       Terra.validates.element();
-//     });
-//   });
-// });
