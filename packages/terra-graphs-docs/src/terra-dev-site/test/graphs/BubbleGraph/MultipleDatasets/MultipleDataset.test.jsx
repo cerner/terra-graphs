bf3a40630db8e310d308e7df39e3cadf5e1d9ea2
Saad Adnan <Saad.Adnan@cerner.com>
2023-05-09 09:35:55 -0500

    updated paths

--- packages/terra-graphs-docs/src/terra-dev-site/test/graphs/BubbleGraph/MultipleDatasets/MultipleDataset.test.jsx
+++ packages/terra-graphs-docs/src/terra-dev-site/test/graphs/BubbleGraph/MultipleDatasets/MultipleDataset.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import utils from '@cerner/carbon-graphs/lib/js/helpers/utils';
 import BubbleMultipleDataset from '@cerner/terra-graphs-docs/lib/terra-graphs-src/components/Bubble/BubbleMultipleDataset';
-import '@cerner/terra-graphs/lib/components/Graph.module.scss';
+import '@cerner/terra-graphs-docs/lib/terra-graphs-src/components/Graph.module.scss';
 import '@cerner/terra-graphs-docs/lib/terra-dev-site/ExampleGraphContainer/ExampleGraphContainer.module.scss';
 import getSimpleAxisData from '@cerner/terra-graphs-docs/lib/example-datasets/graphConfigObjects/Bubble/simpleAxisData';
 import bubbleDataBasic from '@cerner/terra-graphs-docs/lib/example-datasets/dataObjects/Bubble/bubbleDataBasic.js';
