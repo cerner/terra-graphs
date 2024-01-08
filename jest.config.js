32e8b1fd3c2b09a6a42dcde4aae4905fde913b44
Saad <38024451+sdadn@users.noreply.github.com>
2024-01-08 12:31:37 -0600

    updated config

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,13 @@
 const jestConfig = require('@cerner/jest-config-terra');
 
 module.exports = {
-    ...jestConfig,
+  ...jestConfig,
   testMatch: [
     '**/carbon-graphs/tests/unit/controls/Bar/(*.)(test.js)',
-// The patterns below are temporarily commented out as only not all tests are updated to work with Jest.
-// Updating them is currently a work in progress.
-// '**/tests/unit/**/(*.)(test.js)',
-// '**/tests/unit/**/**/(*.)(test.js)',
-    ],
+    // The patterns below are temporarily commented out as only not all tests are updated to work with Jest.
+    // Updating them is currently a work in progress.
+    // '**/tests/unit/**/(*.)(test.js)',
+    // '**/tests/unit/**/**/(*.)(test.js)',
+  ],
 };
-
-
-
 
