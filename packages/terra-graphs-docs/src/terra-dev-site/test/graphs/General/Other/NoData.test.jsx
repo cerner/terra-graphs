052b2c9b9419e1735fbb02d55f2d11bae9279de0
Saad <38024451+sdadn@users.noreply.github.com>
2024-04-02 12:18:17 -0500

    restored file

--- packages/terra-graphs-docs/src/terra-dev-site/test/graphs/General/Other/NoData.test.jsx
+++ packages/terra-graphs-docs/src/terra-dev-site/test/graphs/General/Other/NoData.test.jsx
@@ -1,22 +1,9 @@
 import React from 'react';
-import Carbon from '@cerner/carbon-graphs';
 import utils from '@cerner/carbon-graphs/lib/js/helpers/utils';
-import ExampleGraphContainer from '@cerner/terra-graphs-docs/lib/terra-dev-site/ExampleGraphContainer/ExampleGraphContainer';
+import LineGraph from '@cerner/terra-graphs-docs/lib/terra-graphs-src/components/Line/LineGraph';
+import '@cerner/terra-graphs-docs/lib/terra-dev-site/ExampleGraphContainer/ExampleGraphContainer.module.scss';
 import getGraphConfig from '@cerner/terra-graphs-docs/lib/example-datasets/graphConfigObjects/General/generalDefault';
 
-/*
-Please refer to the documentation below to see the graphConfig and data objects
-*/
+const graphConfig = utils.deepClone(getGraphConfig('#nodata'));
 
-const graphConfig = utils.deepClone(getGraphConfig('#noData'));
-
-const NoDataGeneralExample = () => {
-  React.useEffect(() => {
-    Carbon.api.graph(graphConfig);
-  }, []);
-  return (
-    <ExampleGraphContainer id="noData" />
-  );
-};
-
-export default NoDataGeneralExample;
+export default () => <LineGraph graphID="nodata" graphConfig={graphConfig} />;
