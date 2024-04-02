51775059bb250b9fd85d8cf54c960fce9e168ee9
Saad Adnan <38024451+sdadn@users.noreply.github.com>
2024-04-02 17:30:38 -0500

    Update index.js

--- packages/carbon-graphs/src/index.js
+++ packages/carbon-graphs/src/index.js
@@ -1,6 +1,6 @@
 import Carbon from './js/carbon';
 
-// TODO: update to utilize scss in the src folder
+// TODO: update to pull scss from the src/lib folder instead of dist
 import '../dist/css/carbon-graphs.css';
 
 export default Carbon;
