7ef29100db522e35bba5145a0aedae7498dafe1b
Saad <38024451+sdadn@users.noreply.github.com>
2024-04-02 10:58:48 -0500

    updated index.js

---
+++ packages/carbon-graphs/src/index.js
@@ -0,0 +1,4 @@
+import Carbon from './js/carbon';
+import './less/carbon.less';
+
+export default Carbon;
--- packages/carbon-graphs/src/js/index.js
+++
@@ -1,4 +0,0 @@
-import Carbon from './carbon';
-import '../../dist/css/carbon-graphs.css';
-
-export default Carbon;
