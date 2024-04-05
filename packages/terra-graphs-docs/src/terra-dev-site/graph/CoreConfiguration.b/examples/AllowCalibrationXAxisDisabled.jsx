8b0a8177a1d83d95a93067e760c184d2396480f0
Saad <38024451+sdadn@users.noreply.github.com>
2024-04-05 11:57:55 -0500

    added examples

---
+++ packages/terra-graphs-docs/src/terra-dev-site/graph/CoreConfiguration.b/examples/AllowCalibrationXAxisDisabled.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import Carbon from '@cerner/carbon-graphs';
+
+//  graph configuration object
+
+const graphConfig = {
+  bindTo: '#allow-calibration-disabled-example',
+  axis: {
+    x: {
+      show: true,
+      label: 'x-axis',
+      lowerLimit: 100,
+      upperLimit: 200,
+    },
+    y: {
+      show: true,
+      label: 'y-axis',
+      lowerLimit: -5,
+      upperLimit: 20,
+    },
+  },
+};
+
+//  graph dataset
+
+const dataset1 = {
+  key: 'uid_1',
+  label: {
+    display: 'Data Label 1',
+  },
+  color: Carbon.helpers.COLORS.BLACK,
+  values: [
+    { x: 87, y: -2 },
+    { x: 95, y: 1 },
+    { x: 103, y: -4 },
+    { x: 109, y: -2 },
+    { x: 128, y: 3 },
+    { x: 145, y: 28 },
+    { x: 151, y: 7 },
+    { x: 164, y: 10 },
+    { x: 177, y: 1 },
+    { x: 192, y: 6 },
+    { x: 203, y: -21 },
+    { x: 209, y: -3 },
+    { x: 246, y: 3 },
+  ],
+};
+
+// graph rendering
+
+const NoDataGeneralExample = () => {
+  React.useEffect(() => {
+    const graph = Carbon.api.graph(graphConfig);
+    graph.loadContent(Carbon.api.line(dataset1));
+  }, []);
+  return <div id="allow-calibration-disabled-example" />;
+};
+
+export default NoDataGeneralExample;
