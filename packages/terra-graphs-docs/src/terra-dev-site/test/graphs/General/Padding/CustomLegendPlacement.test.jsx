d0f013cff3380424cf8da61efc93a8d920bbc60c
Saad <38024451+sdadn@users.noreply.github.com>
2024-04-15 13:35:03 -0500

    path updates

--- packages/terra-graphs-docs/src/terra-dev-site/test/graphs/General/Padding/CustomLegendPlacement.test.jsx
+++ packages/terra-graphs-docs/src/terra-dev-site/test/graphs/General/Padding/CustomLegendPlacement.test.jsx
@@ -1,4 +1,4 @@
 import React from 'react';
-import CustomLegendDomExample from '../../../../graph/CoreConfiguration.b/examples/GraphWithCustomLegendDom';
+import CustomLegendDomExample from '../../../../graph/CoreConcepts.b/examples/GraphWithCustomLegendDom';
 
 export default () => <CustomLegendDomExample />;
